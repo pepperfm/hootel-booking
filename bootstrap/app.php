573b7511793e82eab7f936b0d65dbfaf30366c7c
PepperFM <Damon3453@yandex.ru>
2024-05-23 11:41:35 +0300

    fix(request): handle api exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (ModelNotFoundException|NotFoundHttpException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 404);
+        });
     })->create();
